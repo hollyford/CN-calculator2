ee3c766797e3c1c8d9ce9819f01c7d24750bdabe
Holly Ford <holly_ford@hotmail.com>
2021-11-19 18:32:57 +0000

    basic styling added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import { evaluate } from 'mathjs';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+
+const App = () => {
+  const buttons = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "0"]
+  const operators = ["*", "/", "+", "C", "DEL", "="]
+  const [display, setDisplay] = useState("");
+  
+  const numHandler = (value) => {
+      setDisplay(display + value)
+  }
+
+  const opHandler = (value) => {
+    if (value === operators.includes(value) && operators.includes(display.slice(-1))) {
+      return;
+    } else if (value === "=") {
+      setDisplay(evaluate(display))
+    } else if (value === "C") {
+      setDisplay("")
+    } else if(value === "DEL") {
+      const del = display.slice(0, -1);
+      setDisplay(del);
+  } else {
+    setDisplay(display + value)
+  }
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <h2 className="display">{display}</h2>
+        <div className="numbers">
+        {buttons.map((button, index) => {
+          return <button key={index} onClick={() => numHandler(button)} >{button}</button>
+        })}
+        </div>
+        <div className="operators" >
+        {operators.map((operator, index) => {
+          return <button key={index} onClick={() => opHandler(operator)} >{operator}</button>
+        })}
+      </div>
     </div>
-  );
+    </div>
+  )
 }
 
 export default App;
